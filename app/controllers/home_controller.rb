4f51037bbe407270a6bfbca168b13ec104bfe139
Rob Brown <brown4homes@yahoo.com>
2018-11-06 11:29:31 -0800

    Api added

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,13 @@
 class HomeController < ApplicationController
 
   def index
+    require 'net/http'
+    require'json'
+
+    @url = 'https://api.coinmarketcap.com/v2/ticker/'
+    @uri = URI(@url)
+    @response = Net::HTTP.get(@uri)
+    @coins = JSON.parse(@response)
    
   end
 
